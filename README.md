834988fde6c9349e5fa8b90e7573bc91a056c1da
SupunKavinda <supunkavinda1125@gmail.com>
2023-09-12 05:33:58 +0200

    wip

--- README.md
+++ README.md
@@ -3,3 +3,20 @@ This is a Cloudflare Worker to self-serve a [Hyvor Blogs](https://blogs.hyvor.co
 * [Full Tutorial](https://www.hyvor.com/blog/cloudflare-workers-blog)
 
 [![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/hyvor/hyvor-blogs-cloudflare-workers)
+
+## Configuration
+
+You need to configure the following variables in the `wrangler.toml` file.
+
+- `BASE_PATH` - The base path of your blog. For example, if you want to serve your blog at `https://example.com/blog`, set this to `/blog`. If you want to serve at the root, set this to `/`.
+- `SUBDOMAIN` - The subdomain of your blog. Get this from the Hyvor Blogs Console.
+- `DELIVERY_API_KEY` - The Delivery API Key of your blog. Get this from the Hyvor Blogs Console &rarr; Settings &rarr; API Keys.
+- `WEBHOOK_SECRET` - The webhook secret of the webhook sent to `_hb_webhook`. Get this from the Hyvor Blogs Console &rarr; Settings &rarr; Webhooks.
+
+
+## Reserved Routes
+
+These routes are reserved for the worker. You should not create posts in your blog with these paths.
+
+- `{BASE_PATH}/_hb_webhook` (POST only) - Webhook handler for clearing cache
+- `{BASE_PATH}/_cache_health` - Cache (KV) health check
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Blog } from "@hyvor/hyvor-blogs-serve-web";
 
 export interface Env {
 	BASE_PATH: string,
+	SUBDOMAIN: string,
 	DELIVERY_API_KEY: string,
 	WEBHOOK_SECRET: string,
 	BLOG_CACHE: KVNamespace,
@@ -9,6 +10,11 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+
+		const configError = validateConfig(env);
+		if (configError) {
+			return errorResponse(configError)
+		}
 
 		const url = new URL(request.url);
 		const path = url.pathname;
@@ -20,43 +26,73 @@ export default {
 				const json = await request.json();
 				return await blog.handleWebhookRequest(json, env.WEBHOOK_SECRET);
 			} else {
-				return notFound();
+				return notFoundResponse();
 			}
+		}
+
+		if (blogPath === '/_cache_health') {
+			return await cacheHealthResponse(env);
 		}
 
 		return await blog.handleBlogRequest(blogPath);
 	},
 };
 
-function notFound() {
-	return new Response('not found', { status: 404 });
+function validateConfig(env: Env) {
+	if (!env.SUBDOMAIN) {
+		return 'SUBDOMAIN is not set';
+	}
+	if (!env.DELIVERY_API_KEY) {
+		return 'DELIVERY_API_KEY is not set';
+	}
+	return null;
+}
+
+function notFoundResponse() {
+	return new Response('Not found', { status: 404 });
+}
+
+function errorResponse(message: string) {
+	return new Response(message, { status: 500 });
 }
 
 function getBlog(env: Env) {
 
 	return new Blog({
-        subdomain: 'upload-them-etest',
+        subdomain: env.SUBDOMAIN,
         deliveryApiKey: env.DELIVERY_API_KEY,
         webhookSecret: env.WEBHOOK_SECRET,
 
-		cache: {
-			get: async (key: string) => {
-				const val = await env.BLOG_CACHE.get(key);
-				if (!val) return null;
+		cache: getCache(env.BLOG_CACHE)
+	});
 
-				try {
-					return JSON.parse(val);
-				} catch (e) {
-					return null;
-				}
-			},
-			set: async (key: string, value: string) => {
-				await env.BLOG_CACHE.put(key, JSON.stringify(value));
-			},
-			delete: async (key: string) => {
-				await env.BLOG_CACHE.delete(key);
+}
+
+function getCache(namespace: KVNamespace) {
+	return {
+		get: async (key: string) => {
+			const val = await namespace.get(key);
+			if (!val) return null;
+
+			try {
+				return JSON.parse(val);
+			} catch (e) {
+				return null;
 			}
+		},
+		set: async (key: string, value: string) => {
+			await namespace.put(key, JSON.stringify(value));
+		},
+		delete: async (key: string) => {
+			await namespace.delete(key);
 		}
-    });
+	}
+}
 
+async function cacheHealthResponse(env: Env) {
+	const cache = getCache(env.BLOG_CACHE);
+	const random = Math.random().toString(36).substring(7);
+	await cache.set('test', random);
+	const val = await cache.get('test');
+	return new Response(val === random ? 'Healthy ✅' : 'Unhealthy ⚠️', { status: 200 });
 }
